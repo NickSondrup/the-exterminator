ff60e516457d6dafd3a19bfd7846032d20a86e68
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-01 22:56:40 -0600

    auth working. big day tomorrow

--- the-exterminator.client/src/env.js
+++ the-exterminator.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'nicksondrup.us.auth0.com'
+export const clientId = 'hVd9w3Xzqs53QqEvgQpxneB6nmDG4w2r'
+export const audience = 'https://studentauth.com'
